a71694d9149d01c06f5bd475dc10a68383f84673
Scott Dupoy <scott@dupoy.com>
2015-05-22 20:46:27 +0100

    add some initial files

---
+++ FilterFun/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FilterFun
+//
+//  Created by Scott Dupoy on 22/05/2015.
+//  Copyright (c) 2015 Scott Dupoy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
